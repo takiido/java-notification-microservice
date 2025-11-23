525a7c71a3459fbb6f1860c92e4c3430672436c2
takiido <takiid0@proton.me>
2025-11-23 00:02:38 -0600

    Initial commit

---
+++ notification-service/src/main/java/com/takiido/notificationhub/NotificationHubApplication.java
@@ -0,0 +1,13 @@
+package com.takiido.notificationhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationHubApplication.class, args);
+	}
+
+}
---
+++ notification-service/src/test/java/com/takiido/notificationhub/NotificationHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takiido.notificationhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
